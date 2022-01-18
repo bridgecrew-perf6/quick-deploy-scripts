2a05b5c626f75aa9a0a53373e1520b2fe4b152a8
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2022-01-18 15:43:25 -0500

    Increase wait time in webserver setup script

--- setup/setup-internal-webserver.sh
+++ setup/setup-internal-webserver.sh
@@ -10,6 +10,6 @@ export internal_port=80
 export external_port=81
 export script='apt-get update -y && apt-get install curl && curl -s https://github.com/that-github-guy/quick-deploy-scripts/raw/main/scripts/internal-webserver.sh | bash'
 
-export wait=3
+export wait=5
 
 bash ../scripts/lxc.sh
