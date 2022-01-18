d31df5bbc76b6672ee3f7db86a0318ab2a6244e8
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2022-01-18 14:15:29 -0500

    Take bash arguments in script

--- scripts/apache2.sh
+++ scripts/apache2.sh
@@ -7,4 +7,4 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y apache2 certbot python3-certbot-apache
 
-certbot run -n --apache --agree-tos -d $domain -m $mail --redirect
+certbot run -n --apache --agree-tos -d $1 -m $2 --redirect
