49cd9e2dbd5e242d47e1927e382464ab6e2d677c
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2022-01-18 15:11:41 -0500

    Create internal-webserver setup script

--- setup/setup-internal-webserver.sh
+++ setup/setup-internal-webserver.sh
@@ -1,1 +1,13 @@
-placeholder for lxc script
+#!/bin/bash
+set -e
+
+# I am standing over the corpse of best practices
+export instance_name=internal-webserver
+export nesting=false
+export proto=tcp
+export listen_ip=127.0.0.1
+export internal_port=80
+export external_port=81
+export script='apt-get update -y && apt-get install curl && curl -s https://github.com/that-github-guy/quick-deploy-scripts/blob/main/scripts/internal-webserver.sh | bash'
+
+bash ../scripts/lxc.sh
