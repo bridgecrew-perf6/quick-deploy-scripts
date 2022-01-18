d283af9e0d3976a80ccb4543e7f9813f51e4a7d0
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2022-01-18 14:31:35 -0500

    Update lxc.sh

--- scripts/lxc.sh
+++ scripts/lxc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# TODO: make consts uppercase
+# TODO: make it take arguments instead of environment variables
 lxc launch ubuntu:18.04 $instance_name -c security.nesting=$nesting
-lxc config device add $instance_name auto$instance_name proxy listen=tcp:0.0.0.0:$external_port connect=tcp:127.0.0.1:$internal_port
+lxc config device add $instance_name auto$instance_name proxy listen=$proto:$listen_ip:$external_port connect=$proto:127.0.0.1:$internal_port
 lxc exec $instance_name -- $script
